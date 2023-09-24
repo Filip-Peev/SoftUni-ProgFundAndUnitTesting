9ee55e6943a4ef16fa7bbd5b6723c43fec114c55
Filip-Peev <Filip.Peev.Official@gmail.com>
2023-09-25 01:55:08 +0300

    WIP - uploaded 3rd homework

---
+++ 3. Simple Loops - Exercise/03. Old Books/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _03._Old_Books
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
