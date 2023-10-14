81d23c651cfc95877cfc367c189d5a04ac7c5748
Filip-Peev <Filip.Peev.Official@gmail.com>
2023-10-15 02:41:05 +0300

    nested loops added

---
+++ Nested Loops - Lab/03. Triangle of Stars/Program.cs
@@ -0,0 +1,7 @@
+﻿int size = int.Parse(Console.ReadLine());
+
+for (int row = 1; row <= size; row++)
+{
+    string line = new string('*', row);
+    Console.WriteLine(line);
+}
---
+++ Nested Loops - Lab/05. Number Pyramid/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = int.Parse(Console.ReadLine());
+int counter = 1;
+
+for (int row = 1; true; row += 1)
+{
+    for (int col = 1; col <= row; col += 1)
+    {
+        Console.Write($"{counter} ");
+        counter += 1;
+
+        if (counter > n)
+        {
+            break;
+        }
+    }
+
+    Console.WriteLine();
+
+    if (counter > n)
+    {
+        break;
+    }
+}
