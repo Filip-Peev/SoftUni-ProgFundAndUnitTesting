8d21b651b90cd40b21d16111f24223a237084e40
Filip-Peev <Filip.Peev.Official@gmail.com>
2023-11-02 19:48:17 +0200

    arrays lab added

---
+++ Arrays - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+string[] weekDay = {
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+};
+if (n >= 1 && n <= 7)
+{
+    Console.WriteLine(weekDay[n-1]);
+}
+else
+{
+    Console.WriteLine("Invalid day!");
+}
