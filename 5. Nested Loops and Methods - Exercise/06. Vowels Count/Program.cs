77e91d1475deb44ed59ebd4c2e62321083a80824
Filip-Peev <Filip.Peev.Official@gmail.com>
2023-11-02 04:47:49 +0200

    added 5th exercise

---
+++ 5. Nested Loops and Methods - Exercise/06. Vowels Count/Program.cs
@@ -0,0 +1,20 @@
+﻿string text = Console.ReadLine();
+
+Console.WriteLine(GetVowelsCount(text));
+static int GetVowelsCount(string text)
+{
+    int count = 0;
+    for (int position = 0; position <= text.Length - 1; position++)
+    {
+        char symbol = text[position];
+        if (symbol == 'a' || symbol == 'A' ||
+            symbol == 'e' || symbol == 'E' ||
+            symbol == 'i' || symbol == 'I' ||
+            symbol == 'o' || symbol == 'O' ||
+            symbol == 'u' || symbol == 'U')
+        {
+            count++;
+        }
+    }
+    return count;
+}
---
+++ 5. Nested Loops and Methods - Exercise/07. Factorial Division/Program.cs
@@ -0,0 +1,17 @@
+﻿int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+
+static int CalculateFactorial(int num)
+{
+	int factorial = 1;
+	for (int i = 1; i <= num; i++)
+	{
+		factorial *= i;
+	}
+	return factorial;
+}
+
+int num1 = CalculateFactorial(firstNumber);
+int num2 = CalculateFactorial(secondNumber);
+
+Console.WriteLine(num1/num2);
