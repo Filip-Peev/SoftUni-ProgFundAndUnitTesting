99008e26df793ba63c08410b49d857dd1f675dc9
Filip-Peev <Filip.Peev.Official@gmail.com>
2023-11-04 17:28:35 +0200

    added more files

---
+++ 6. Arrays and Lists - Exercise/08. Array Rotation/Program.cs
@@ -0,0 +1,15 @@
+﻿List<int> input = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+int rotations = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < rotations; i++)
+{
+    int firstNum = input[0];
+    input.RemoveAt(0);
+    input.Add(firstNum);
+}
+
+Console.WriteLine(string.Join(" ", input));
